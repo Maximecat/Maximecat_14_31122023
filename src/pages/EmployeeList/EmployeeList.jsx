4b9d6227df3435fc9f6ed4d3d5128ad9b070c5f5
Maxime CAT <maximecat@outlook.fr>
2024-01-01 13:42:12 +0100

    Page list employee

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -2,8 +2,8 @@ import './EmployeeList.css';
 
 function EmployeeList() {
     return (
-        <div className="EmployeeList">
-
+        <div className="container">
+            <h1 className='current-employees'>Current Employees</h1>
         </div>
     );
 }
