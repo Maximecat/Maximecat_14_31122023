596b9f121e36f03e335cf4e89c89f89ce6a4f11e
Maxime CAT <maximecat@outlook.fr>
2024-01-01 13:41:25 +0100

    homepage create employee

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -2,8 +2,12 @@ import './CreateEmployee.css';
 
 function CreateEmployee() {
   return (
-    <div className="CreateEmployee">
-
+    <div className="main">
+      <h1 className='hrnet-title'>HRnet</h1>
+      <div className='container'>
+        <a className='link-page-employees' href="/employees">View Current Employees</a>
+        <h2 className='create-employee'>Create Employee</h2>
+      </div>
     </div>
   );
 }
