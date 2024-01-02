4532c91e781fd706f63a6a43ebcf43dd2f7092cc
Maxime CAT <maximecat@outlook.fr>
2024-01-02 16:10:44 +0100

    CreateEmployee page, add state for recover input value

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -6,14 +6,23 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DateField } from '@mui/x-date-pickers';
 import DropdownState from '../../components/DropdownState/DropdownState';
 import DropdownDepartment from '../../components/DropdownDepartment/DropdownDepartment';
+import { useState } from 'react';
 
 function CreateEmployee() {
 
-  const firstname = document.getElementById('firstname')
-
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [street, setStreet] = useState("")
+  const [city, setCity] = useState("")
+  const [zipcode, setZipCode] = useState("")
 
   const submit = (e) => {
     e.preventDefault()
+    console.log(firstname);
+    console.log(lastname);
+    console.log(street);
+    console.log(city);
+    console.log(zipcode);
   }
 
   return (
@@ -22,11 +31,12 @@ function CreateEmployee() {
       <a className='link-page-employees' href="/employees">View Current Employees</a>
       <h2 className='create-employee'>Create Employee</h2>
       <form onSubmit={submit}>
-        <TextField id="firstname" label="First Name" value={firstname} variant="outlined" style={{ marginBottom: "15px", marginTop: "25px" }} />
-        <TextField id="lastname" label="Last Name" variant="outlined" />
+        <TextField id="firstname" label="First Name" type='text' value={firstname} variant="outlined" style={{ marginBottom: "15px", marginTop: "25px" }} onChange={(e) => setFirstname(e.target.value)} />
+        <TextField id="lastname" label="Last Name" type='text' value={lastname} variant="outlined" onChange={(e) => setLastname(e.target.value)} />
 
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateField label="Date of Birth"
+          <DateField
+            label="Date of Birth"
             slotProps={{
               textField: {
                 helperText: 'MM/DD/YYYY',
@@ -46,10 +56,10 @@ function CreateEmployee() {
 
         <Box style={{ border: "2px solid black", borderRadius: "5px", marginTop: "10px", padding: "15px", display: "flex", flexDirection: "column" }} >
           <h3 className='address-title'>Address</h3>
-          <TextField id="outlined-basic" label="Street" variant="outlined" style={{ marginBottom: "15px" }} />
-          <TextField id="outlined-basic" label="City" variant="outlined" style={{ marginBottom: "15px" }} />
+          <TextField id="outlined-basic" label="Street" type='text' value={street} variant="outlined" style={{ marginBottom: "15px" }} onChange={(e) => setStreet(e.target.value)} />
+          <TextField id="outlined-basic" label="City" type='text' value={city} variant="outlined" style={{ marginBottom: "15px" }} onChange={(e) => setCity(e.target.value)} />
           <DropdownState />
-          <TextField id="outlined-basic" label="Zip Code" variant="outlined" style={{ marginTop: "15px" }} />
+          <TextField id="outlined-basic" label="Zip Code" type='text' value={zipcode} variant="outlined" style={{ marginTop: "15px" }} onChange={(e) => setZipCode(e.target.value)} />
         </Box>
 
         <DropdownDepartment />
