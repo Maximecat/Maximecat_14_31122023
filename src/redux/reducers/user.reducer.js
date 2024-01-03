0dd907359979bbaed840efcf3737a2836810ba9a
Maxime CAT <maximecat@outlook.fr>
2024-01-03 16:52:36 +0100

    user.reducer add users Array and modal/handle

--- src/redux/reducers/user.reducer.js
+++ src/redux/reducers/user.reducer.js
@@ -2,30 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 import { saveAction } from "../actions/user.action";
 
 const initialState = {
-    firstname: "",
-    lastname: "",
-    birthDate: "",
-    startDate: "",
-    street: "",
-    city: "",
-    state: "",
-    zipCode: "",
-    department: ""
+    users: [],
+    done: false
 };
+
+
 
 const userSlice = createSlice({
     initialState,
     name: "userState",
+    reducers: {
+        'modal/handle': (state, action) => {
+            state.done = action.payload
+        }
+    },
     extraReducers(builder) {
         builder
-            .addCase(saveAction.pending, (state) => {
-                console.log("pending");
-            })
             .addCase(saveAction.fulfilled, (state, action) => {
                 console.log("fulfilled");
-            })
-            .addCase(saveAction.rejected, (state, action) => {
-                console.log("rejected");
+                state.users = [...state.users, action.payload]
+                state.done = true
             })
     }
 })
