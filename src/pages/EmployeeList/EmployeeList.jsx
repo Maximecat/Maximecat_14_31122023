9a2ae0d9fe8ac5d01ac74df440e44af1207f537f
Maxime CAT <maximecat@outlook.fr>
2024-01-03 16:54:34 +0100

    EmployeeList page change row and column rendering and pagination

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -1,70 +1,90 @@
 import { DataGrid } from '@mui/x-data-grid';
 import './EmployeeList.css';
+import { useSelector } from 'react-redux';
+import dayjs from 'dayjs';
+import { Link } from 'react-router-dom';
 
 function EmployeeList() {
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
         {
-            field: 'firstName',
+            field: 'firstname',
             headerName: 'First name',
-            width: 150,
-            editable: true,
+            type: 'string',
+            width: 120
         },
         {
-            field: 'lastName',
+            field: 'lastname',
             headerName: 'Last name',
-            width: 150,
-            editable: true,
+            type: 'string',
+            width: 120
         },
         {
-            field: 'age',
-            headerName: 'Age',
+            field: 'startDate',
+            headerName: 'Start Date',
+            type: 'string',
+            width: 120,
+            valueGetter: (params) => dayjs(params.row.startDate).format('MM/DD/YYYY')
+        },
+        {
+            field: 'department',
+            headerName: 'Department',
+            type: 'string',
+            width: 120
+        },
+        {
+            field: 'birthDate',
+            headerName: 'Date of Birth',
+            type: 'string',
+            width: 120,
+            valueGetter: (params) => dayjs(params.row.birthDate).format('MM/DD/YYYY')
+        },
+        {
+            field: 'street',
+            headerName: 'Street',
+            type: 'string',
+            width: 150
+        },
+        {
+            field: 'city',
+            headerName: 'City',
+            type: 'string',
+            width: 150
+        },
+        {
+            field: 'state',
+            headerName: 'State',
+            type: 'string',
+            width: 120
+        },
+        {
+            field: 'zipcode',
+            headerName: 'Zip Code',
             type: 'number',
-            width: 110,
-            editable: true,
-        },
-        {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            valueGetter: (params) =>
-                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
+            width: 100
+        }
     ];
 
-    const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
+    const rows = useSelector((state) => state.user.users);
 
     return (
         <div className="main-container">
             <h1 className='current-employees'>Current Employees</h1>
             <DataGrid
+                getRowId={(row) => row.id}
                 rows={rows}
                 columns={columns}
                 initialState={{
                     pagination: {
                         paginationModel: {
-                            pageSize: 5,
+                            pageSize: 10 || 25 || 50 || 100
                         },
                     },
                 }}
-                pageSizeOptions={[5]}
-                checkboxSelection
+                pageSizeOptions={[10, 25, 50, 100]}
                 disableRowSelectionOnClick
             />
-            <a href="/">Home</a>
+            <Link to="/" className='link-to-home'>Home</Link>
         </div>
     );
 }
